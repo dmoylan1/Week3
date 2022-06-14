d23951443e6e24225771b0710dca044382b654d5
Danielle Moylan <dmmoylan@sau29.org>
2022-06-13 21:51:35 -0400

    arrays video practice

---
+++ index.js
@@ -0,0 +1,15 @@
+var colors = [];
+colors.push('Red');
+colors.push('Orange');
+colors.push('Yellow');
+colors.push('Green');
+colors.push('Blue');
+colors.push('Purple');
+
+for (let i = 0; i < colors.length; i++) {
+    console.log(colors[i]);
+}
+
+for (names of colors) {
+    console.log(names);
+}
