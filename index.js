d9e4b59fc4e517cec075aa5437dfdaf2c0e244a5
Danielle Moylan <dmmoylan@sau29.org>
2022-06-14 09:28:55 -0400

    Maps

--- index.js
+++ index.js
@@ -1,15 +1,38 @@
-var colors = [];
-colors.push('Red');
-colors.push('Orange');
-colors.push('Yellow');
-colors.push('Green');
-colors.push('Blue');
-colors.push('Purple');
+//  *** Arrays ***
 
-for (let i = 0; i < colors.length; i++) {
-    console.log(colors[i]);
-}
+// var colors = [];
+// colors.push('Red');
+// colors.push('Orange');
+// colors.push('Yellow');
+// colors.push('Green');
+// colors.push('Blue');
+// colors.push('Purple');
 
-for (names of colors) {
-    console.log(names);
-}
+// for (let i = 0; i < colors.length; i++) {
+//     console.log(colors[i]);
+// }
+
+// for (names of colors) {
+//     console.log(names);
+// }
+
+
+// *** Functions ***
+
+// function myFunction () {
+//     for (i=0; i<100; i++) {
+//         console.log(i);
+//     }
+// }
+
+// myFunction();
+
+// *** Intermediate Array Methods ***
+// *** Maps *** 
+
+let names = ['Danielle', 'Dan', 'Chloe', 'Jacob'];
+let lengths = names.map(function(element) {
+    return element.length;
+});
+
+console.log(lengths);
